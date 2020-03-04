d668946aefb74ce66ac0a5c4e3555442252b54e6
Darko Draskovic <darko.draskovic@gmail.com>
2020-03-04 23:48:01 +0100

    Add createShapes() to sketch 09
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- src/week09/sketch/Sketch09.java
+++ src/week09/sketch/Sketch09.java
@@ -6,11 +6,10 @@ import java.util.Iterator;
 
 import processing.core.PApplet;
 import processing.core.PShape;
-import processing.event.KeyEvent;
 import week09.lib.Shape;
 
 public class Sketch09 extends PApplet {
-	
+
 	ArrayList<Shape> shapes = new ArrayList<Shape>();
 	HashMap<String, Integer> colors = new HashMap<String, Integer>();
 
@@ -18,29 +17,38 @@ public class Sketch09 extends PApplet {
 		size(600, 600);
 	}
 
+	float sclIncr = 0.2f;
 	float lineScl = 2f;
 	float gapScl = 2f;
 	float weightScl = 2f;
-	
+	int verDist;
+	int distScl = 30;
+
 	@Override
 	public void setup() {
 		colors.put("green", 0xFF99B898);
 		colors.put("red", 0xFFE84A5F);
 
-		int verDist = 20;
+		createShapes();
+	}
+
+	private void createShapes() {
+		shapes.clear();
+
+		verDist = height / distScl;
 		int numLineRows = height / verDist;
 
+		float noiseArg = 0.1f;
 		float lineLen = width / 8;
 		float gapLen = width / 16;
-		
-		float noiseArg = 0.1f;
-		for (int i = 0; i < numLineRows; i++) {
+
+		for (int i = 0; i <= numLineRows; i++) {
 			int lineStart = 0;
 			while (lineStart < width) {
 				float noise = noise(noiseArg);
 				float ll = lineLen * noise * lineScl;
 				float gl = gapLen * noise * gapScl;
-				
+
 				PShape line = createShape(LINE, 0, 0, ll, 0);
 				Shape shape = new Shape(this, line);
 				shape.position.y = i * verDist;
@@ -48,7 +56,7 @@ public class Sketch09 extends PApplet {
 				shape.strokeColor = colors.get("red");
 				shape.strokeWeight = 1 + weightScl * noise;
 				shapes.add(shape);
-				
+
 				lineStart += ll + gl;
 				noiseArg += 0.1;
 			}
@@ -56,9 +64,45 @@ public class Sketch09 extends PApplet {
 	}
 
 	@Override
+	public void keyPressed() {
+		super.keyPressed();
+
+		switch (key) {
+		case 'q':
+			lineScl += sclIncr;
+			break;
+		case 'a':
+			lineScl -= sclIncr;
+			break;
+		case 'w':
+			gapScl += sclIncr;
+			break;
+		case 's':
+			gapScl -= sclIncr;
+			break;	
+		case 'e':
+			weightScl += sclIncr;
+			break;
+		case 'd':
+			weightScl -= sclIncr;
+			break;
+		case 'r':
+			distScl--;
+			break;
+		case 'f':
+			distScl++;
+			break;			
+		default:
+			break;
+		}
+
+		createShapes();
+	}
+	
+
+	@Override
 	public void draw() {
 		background(colors.get("green"));
-		
 
 		for (Iterator<Shape> iterator = shapes.iterator(); iterator.hasNext();) {
 			Shape shape = (Shape) iterator.next();
